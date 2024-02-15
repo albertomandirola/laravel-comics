5df6f13c1d1d3fd2d0b22ac6874590738cc13d4b
albertomandirola <albymandi00@gmail.com>
2024-02-15 16:46:11 +0100

    footer done

--- config/footer.php
+++ config/footer.php
@@ -6,6 +6,10 @@ return [
         'list_links' => ["Characters", "Comics", "Movies", "Tv", "Games", "Videos", "News"],
     ],
     [
+        'list_title' => "SHOP",
+        'list_links' => ["Shop DC", "Shop DC Collectibles"],
+    ],
+    [
         'list_title' => "DC",
         'list_links' => ["Terms Of Use", "Privacy policy (New)", "Ad Choices", "Advertising", "Jobs", "Subscriptions", "Talent Workshops", "CPSC Certificates", "Ratings", "Shop Help", "Contact Us"],
     ],
@@ -13,9 +17,6 @@ return [
         'list_title' => "SITES",
         'list_links' => ["DC", "MAD Magazine", "DC Kids", "DC Universe", "DC Power Visa"],
     ],
-    [
-        'list_title' => "SHOP",
-        'list_links' => ["Shop DC", "Shop DC Collectibles"],
-    ],
+    
 
 ];
