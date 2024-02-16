ad7288ae2065f4281a6691fef44831ba23dcf7ab
albertomandirola <albymandi00@gmail.com>
2024-02-16 10:42:24 +0100

    add button in card for go too details

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -5,11 +5,13 @@
         <div class="row">
             @foreach ($comics as $comic)
                 <div class="col-2 my-3">
-                    <div class="card" style="width: 100%; height:300px">
+                    <div class="card" style="width: 100%; height:330px">
                         <img style="height: 200px;" src="{{ $comic['thumb'] }}" class="card-img-top"
                             alt="{{ $comic['title'] }}">
                         <div class="card-body">
                             <h5 class="card-title">{{ $comic['title'] }}</h5>
+                            <a href="{{ route('comic_details', ['param' => $comic['id']]) }}"
+                                class=" btn btn-sm btn-primary">details</a>
                         </div>
                     </div>
                 </div>
